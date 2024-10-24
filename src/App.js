361e9910f424de2229beccb59f231f1e8a6df6d1
alja1313 <olgaslahota@gmail.com>
2024-10-24 18:44:08 +0200

    Added the Footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/olga-slahota-485359162"
+          target="_blank"
+        >
+          Olga Slahota
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/alja1313/react-weather-application"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
