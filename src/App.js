3a7b065cbd0bb5cd8fdfb90e46fcbb29dcf78a86
alja1313 <olgaslahota@gmail.com>
2024-10-24 19:17:39 +0200

    Added Boostrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/olga-slahota-485359162"
-          target="_blank"
-        >
-          Olga Slahota
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/alja1313/react-weather-application"
-          target="_blank"
-        >
-          GitHub 🌡
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/olga-slahota-485359162"
+            target="_blank"
+          >
+            Olga Slahota
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/alja1313/react-weather-application"
+            target="_blank"
+          >
+            GitHub 🌡
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
